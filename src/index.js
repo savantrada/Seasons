511cdeeb0d4370e2331f69def0cc5e7172db9f8b
savantrada <savantrada1022@gmail.com>
2018-12-22 16:48:24 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () => {
-    return <div>
-       hi there
-    </div>;
+class App extends React.Component{
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props);
+
+        this.state= {latitude: 5}; 
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({latitude: position.coords.latitude})
+            },
+            (err) => console.log(err)
+        );
+    }
+    render(){ 
+        
+
+        return <div>
+            latitude {this.state.latitude}
+            </div>;
+    }
 }
 ReactDOM.render(<App/>,document.querySelector('#root'));
